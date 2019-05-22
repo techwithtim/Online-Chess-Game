70747366d845fadc33155ccbe1e45cef535c1d8b
Tech With Tim <35348871+techwithtim@users.noreply.github.com>
2019-05-22 11:20:06 -0400

    Add files via upload
    added get_pip.py file and a script that tries to install pygame on launch if it is not found

--- game.py
+++ game.py
@@ -1,3 +1,35 @@
+import subprocess
+import sys
+import get_pip
+
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+try:
+    print("[GAME] Trying to import pygame")
+    import pygame
+except:
+    print("[EXCEPTION] Pygame not installed")
+
+    try:
+        print("[GAME] Trying to install pygame via pip")
+        import pip
+        install("pygame")
+        print("[GAME] Pygame has been installed")
+    except:
+        print("[EXCEPTION] Pip not installed on system")
+        print("[GAME] Trying to install pip")
+        get_pip.main()
+        print("[GAME] Pip has been installed")
+        try:
+            print("[GAME] Trying to install pygame")
+            import pip
+            install("pygame")
+            print("[GAME] Pygame has been installed")
+        except:
+            print("[ERROR 1] Pygame could not be installed")
+
+
 import pygame
 import os
 import time
