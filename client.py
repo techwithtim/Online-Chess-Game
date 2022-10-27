ce70652b150ae2f7f956e1fbc23a8283f7e09da4
yashwanth kiran <yashwanthkiran2624@gmail.com>
2022-10-27 16:46:14 +0530

    commit

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = "localhost"
+        #self.host = "192.168.8.60"
         self.port = 5555
         self.addr = (self.host, self.port)
         self.board = self.connect()
