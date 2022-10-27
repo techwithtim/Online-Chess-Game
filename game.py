cab5879511792570e047ea67b6a37eb5b7960be1
yashwanth kiran <yashwanthkiran2624@gmail.com>
2022-10-28 02:25:26 +0530

    commit

--- game.py
+++ game.py
@@ -1,15 +1,8 @@
-'''
-the main game
-author:@techwithtim
-requirements:see requirements.txt
-'''
-
+import pygame
 import subprocess
 import sys
 import get_pip
 
-def install(package):
-    subprocess.call([sys.executable, "-m", "pip", "install", package])
 
 try:
     print("[GAME] Trying to import pygame")
@@ -43,9 +36,10 @@ from client import Network
 import pickle
 pygame.font.init()
 
-board = pygame.transform.scale(pygame.image.load(os.path.join("img","board_alt.png")), (750, 750))
+board = pygame.transform.scale(pygame.image.load(
+    os.path.join("img", "board_alt.png")), (750, 750))
 chessbg = pygame.image.load(os.path.join("img", "chessbg.png"))
-rect = (113,113,525,525)
+rect = (113, 113, 525, 525)
 
 turn = "w"
 
@@ -56,11 +50,12 @@ def menu_screen(win, name):
     offline = False
 
     while run:
-        win.blit(chessbg, (0,0))
+        win.blit(chessbg, (0, 0))
         small_font = pygame.font.SysFont("comicsans", 50)
-        
+
         if offline:
-            off = small_font.render("Server Offline, Try Again Later...", 1, (255, 0, 0))
+            off = small_font.render(
+                "Server Offline, Try Again Later...", 1, (255, 0, 0))
             win.blit(off, (width / 2 - off.get_width() / 2, 500))
 
         pygame.display.update()
@@ -82,25 +77,26 @@ def menu_screen(win, name):
                     offline = True
 
 
-    
 def redraw_gameWindow(win, bo, p1, p2, color, ready):
     win.blit(board, (0, 0))
     bo.draw(win, color)
 
-    formatTime1 = str(int(p1//60)) + ":" + str(int(p1%60))
+    formatTime1 = str(int(p1//60)) + ":" + str(int(p1 % 60))
     formatTime2 = str(int(p2 // 60)) + ":" + str(int(p2 % 60))
-    if int(p1%60) < 10:
+    if int(p1 % 60) < 10:
         formatTime1 = formatTime1[:-1] + "0" + formatTime1[-1]
-    if int(p2%60) < 10:
+    if int(p2 % 60) < 10:
         formatTime2 = formatTime2[:-1] + "0" + formatTime2[-1]
 
     font = pygame.font.SysFont("comicsans", 30)
     try:
-        txt = font.render(bo.p1Name + "\'s Time: " + str(formatTime2), 1, (255, 255, 255))
-        txt2 = font.render(bo.p2Name + "\'s Time: " + str(formatTime1), 1, (255,255,255))
+        txt = font.render(bo.p1Name + "\'s Time: " +
+                          str(formatTime2), 1, (255, 255, 255))
+        txt2 = font.render(bo.p2Name + "\'s Time: " +
+                           str(formatTime1), 1, (255, 255, 255))
     except Exception as e:
         print(e)
-    win.blit(txt, (520,10))
+    win.blit(txt, (520, 10))
     win.blit(txt2, (520, 700))
 
     txt = font.render("Press q to Quit", 1, (255, 255, 255))
@@ -140,7 +136,7 @@ def redraw_gameWindow(win, bo, p1, p2, color, ready):
 def end_screen(win, text):
     pygame.font.init()
     font = pygame.font.SysFont("comicsans", 80)
-    txt = font.render(text,1, (255,0,0))
+    txt = font.render(text, 1, (255, 0, 0))
     win.blit(txt, (width / 2 - txt.get_width() / 2, 300))
     pygame.display.update()
 
@@ -250,14 +246,14 @@ def main():
                 if event.key == pygame.K_LEFT:
                     bo = n.send("back")
 
-
             if event.type == pygame.MOUSEBUTTONUP and color != "s":
                 if color == bo.turn and bo.ready:
                     pos = pygame.mouse.get_pos()
                     bo = n.send("update moves")
                     i, j = click(pos)
-                    bo = n.send("select " + str(i) + " " + str(j) + " " + color)
-    
+                    bo = n.send("select " + str(i) +
+                                " " + str(j) + " " + color)
+
     n.disconnect()
     bo = 0
     menu_screen(win)
